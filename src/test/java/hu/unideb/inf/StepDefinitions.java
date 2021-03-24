4d70e5c63e13e733dbaae35cd37950e0e7b568cc
RolandMajor <majorsandorroland@gmail.com>
2021-03-24 15:44:07 +0100

    Adding everything

--- src/test/java/hu/unideb/inf/StepDefinitions.java
+++ src/test/java/hu/unideb/inf/StepDefinitions.java
@@ -5,10 +5,15 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.Assert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.concurrent.TimeUnit;
 
 import static org.junit.Assert.*;
@@ -17,30 +22,32 @@ public class StepDefinitions {
 
     private static final int WAIT_TIME = 10;
 
-    private static HomePage homePage;
+    static WebDriver driver;
 
-    private static WebDriver driver;
+    private HomePage homePage;
 
     static {
         WebDriverManager.chromedriver().setup();
         ChromeOptions options = new ChromeOptions();
+        //options.setHeadless(true);
+        //options.addArguments("--no-sandbox");
         driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(WAIT_TIME, TimeUnit.SECONDS);
     }
 
-    @Given("The AutomationPractice site is open")
-    public void theAutomationPracticeSiteIsOpen() {
+    @Given("the home page is opened")
+    public void theHomePageIsOpened() {
         homePage = new HomePage(driver);
     }
 
-    @And("The Sign In link is clicked")
+    @And("the Sign In link is clicked")
     public void theSignInLinkIsClicked() {
-        homePage.clickOnSignInLink();
+        homePage.clickSignInLink();
     }
 
-    @When("The Sign In button is clicked")
+    @Given("the Sign In button is clicked")
     public void theSignInButtonIsClicked() {
-        homePage.clickOnSignInButton();
+        homePage.clickSignInButton();
     }
 
     @Given("The {string} is filled with {string}")
@@ -48,7 +55,13 @@ public class StepDefinitions {
         homePage.fillField(field, parameter);
     }
 
-    @Then("The {string} message is shown")
-    public void theMsgMessageIsShown(String msg) {
+    @Then("The {string} error message is shown")
+    public void theMsgErrorMessageIsShown(String msg) {
+        Optional<String> errorMessage = homePage.getErrorMessage();
+        if (errorMessage.isPresent()) {
+            Assert.assertEquals(msg, errorMessage.get());
+        } else {
+            fail();
+        }
     }
 }
